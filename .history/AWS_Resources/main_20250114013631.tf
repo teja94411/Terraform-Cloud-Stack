dd2d1649825b3bcfddd8727ed02fb53165d3a889
sai <sai@gmail.com>
2025-02-04 14:34:15 +0530

    removing blank files

--- .history/AWS_Resources/main_20250114013631.tf
+++
@@ -1,7 +0,0 @@
-module "vpc" {
-  source = "./modules/vpc"
-}
-
-module "ec2" {
-  source = "./modules/ec2"
-}
--- .history/AWS_Resources/modules/ec2/main_20250127103940.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "webserver" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    tags = {
-      name = "webserver"
-    }     
-    key_name      = "linux"  
-
-}
