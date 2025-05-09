56f70bbc580740468c3cbfb8a07af639ec0f4e64
sai <sai@gmail.com>
2025-05-10 02:48:47 +0530

    modifying the files

--- AWS_Resources/modules/iam/main.tf
+++ AWS_Resources/modules/iam/main.tf
@@ -14,13 +14,13 @@ resource "aws_iam_group_policy_attachment" "s3_full_access" {
 
 resource "aws_iam_user" "new_iam_user" {
   for_each = toset(var.aws_iam_user)  
-  name     = each.value  # Referencing each user name correctly
+  name     = each.value
 }
 
 resource "aws_iam_group_membership" "add_user_to_group" {
-  name  = "group-membership-${aws_iam_group.group_name.name}"  # Unique name for the membership
+  name  = "group-membership-${aws_iam_group.group_name.name}"
   group = aws_iam_group.group_name.name
-  users = tolist([for user in aws_iam_user.new_iam_user : user.name])  # Extracting list of usernames
+  users = tolist([for user in aws_iam_user.new_iam_user : user.name])
 }
 
 resource "aws_iam_policy" "deny_ec2_s3_policy" {
@@ -28,4 +28,3 @@ resource "aws_iam_policy" "deny_ec2_s3_policy" {
   description = var.policy_description
   policy      = var.policy_json
 }
-
